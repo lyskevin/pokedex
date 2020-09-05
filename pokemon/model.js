88bcf0e9ecce249925c1ddb76390f225af7029a6
Kevin Lim <lyskevin@gmail.com>
2020-09-05 16:48:39 +0800

    Add controller.js and model.js in the pokemon folder

---
+++ pokemon/model.js
@@ -0,0 +1,34 @@
+// Model contains the app's business logic
+
+let pokemonTypes = new Set(['Normal', 'Fire', 'Fighting', 'Water',
+  'Flying', 'Grass', 'Poison', 'Electric', 'Ground', 'Psychic', 'Rock',
+  'Ice', 'Bug', 'Dragon', 'Ghost', 'Dark', 'Steel', 'Fairy']);
+
+var mongoose = require('mongoose');
+
+// Setup schema
+var pokemonSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  pokedexNumber: {
+    type: Number,
+    required: true
+  },
+  primaryType: {
+    type: String,
+    required: true
+  },
+  secondaryType: {
+    type: String,
+  }
+})
+
+// Export pokemon model
+
+var pokemon = module.exports = mongoose.model('pokemon', pokemonSchema);
+
+module.exports.get = function (callback, limit) {
+  pokemon.find(callback).limit(limit);
+}
