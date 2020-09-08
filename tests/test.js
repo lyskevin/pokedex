d1563039519bf4b06ce0a782ad1dcd810f2efd1f
Kevin Lim <lyskevin@gmail.com>
2020-09-09 00:28:40 +0800

    Update test.js to test POST, PUT, and DELETE requests

--- tests/test.js
+++ tests/test.js
@@ -7,69 +7,108 @@ var app = require('../index')
 chai.use(chaiHttp);
 chai.should();
 
-// Configure mock database
-var mongodb = require('mongo-mock');
-var MongoClient = mongodb.MongoClient;
-var url = 'mongodb://localhost/pokedex';
-var docs = [
-  {
-    "name": "Bulbasaur",
-    "pokedexNumber": 1,
-    "primaryType": "Grass",
-    "secondaryType": "Poison"
-  },
-  {
-    "name": "Ivysaur",
-    "pokedexNumber": 2,
-    "primaryType": "Grass",
-    "secondaryType": "Poison"
-  },
-  {
-    "name": "Venusaur",
-    "pokedexNumber": 3,
-    "primaryType": "Grass",
-    "secondaryType": "Poison"
-  }
-];
-
-MongoClient.connect(url).then(client => {
-  var collection = client.collection('pokemon');
-  collection.insertMany(docs, function (err, result) {
-    // Close connection
-    client.close();
-  });
-});
-
 describe('Pokemon', () => {
-  describe('GET', () => {
+  describe('POST', () => {
+      // Test to add Blaziken
+      it("should add Blaziken", function(done) {
+        chai.request(app)
+          .post('/api/pokemon')
+          .set('content-type', 'application/json')
+          .send({
+            "name": "Blaziken",
+            "pokedexNumber": 257,
+            "primaryType": "Fire",
+            "secondaryType": "Fighting"
+          })
+          .end((error, result) => {
+            result.should.have.status(200);
+            result.body.message.should.equal('New Pokemon created!');
+            result.body.data.should.have.property('name');
+            result.body.data.should.have.property('pokedexNumber');
+            result.body.data.should.have.property('primaryType');
+            result.body.data.should.have.property('secondaryType');
+            result.body.data.name.should.equal('Blaziken');
+            result.body.data.pokedexNumber.should.equal(257);
+            result.body.data.primaryType.should.equal('Fire');
+            result.body.data.secondaryType.should.equal('Fighting');
+            done();
+          });
+      });
+  });
 
+  describe('GET', () => {
     // Test to get all Pokemon
     it("should get all Pokemon", function(done) {
-      this.timeout(120000);
       chai.request(app)
         .get('/api/pokemon')
         .end((error, result) => {
           result.should.have.status(200);
+          result.body.message.should.equal('Pokemon retrieved successfully');
           result.body.should.be.a('object');
           done();
         });
     });
 
     // Test to get a single Pokemon
-    it("should get Bulbasaur", function(done) {
-      this.timeout(120000);
+    it("should get Blaziken", function(done) {
       chai.request(app)
-        .get('/api/pokemon/Bulbasaur')
+        .get('/api/pokemon/Blaziken')
         .end((error, result) => {
           result.should.have.status(200);
+          result.body.message.should.equal('Blaziken details');
           result.body.data.should.have.property('name');
           result.body.data.should.have.property('pokedexNumber');
           result.body.data.should.have.property('primaryType');
           result.body.data.should.have.property('secondaryType');
-          result.body.data.name.should.equal('Bulbasaur');
-          result.body.data.pokedexNumber.should.equal(1);
-          result.body.data.primaryType.should.equal('Grass');
-          result.body.data.secondaryType.should.equal('Poison');
+          result.body.data.name.should.equal('Blaziken');
+          result.body.data.pokedexNumber.should.equal(257);
+          result.body.data.primaryType.should.equal('Fire');
+          result.body.data.secondaryType.should.equal('Fighting');
+          done();
+        });
+    });
+  });
+
+  describe('PUT', () => {
+    // Test to update Blaziken's details
+    it("should update Blaziken's details", function(done) {
+      chai.request(app)
+        .put('/api/pokemon/Blaziken')
+        .set('content-type', 'application/json')
+        .send({
+          "name": "Blaziken",
+          "pokedexNumber": 400,
+          "primaryType": "Fire",
+          "secondaryType": "Water"
+        })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.message.should.equal('Pokemon details updated');
+          result.body.data.should.have.property('name');
+          result.body.data.should.have.property('pokedexNumber');
+          result.body.data.should.have.property('primaryType');
+          result.body.data.should.have.property('secondaryType');
+          result.body.data.name.should.equal('Blaziken');
+          result.body.data.pokedexNumber.should.equal(400);
+          result.body.data.primaryType.should.equal('Fire');
+          result.body.data.secondaryType.should.equal('Water');
+          done();
+        });
+    });
+  });
+
+  describe('DELETE', () => {
+    // Test to delete Blaziken
+    it("should delete Blaziken", function(done) {
+      chai.request(app)
+        .delete('/api/pokemon/Blaziken')
+        .set('content-type', 'application/json')
+        .send({
+          "name": "Blaziken",
+        })
+        .end((error, result) => {
+          result.should.have.status(200);
+          result.body.message.should.equal('Pokemon deleted');
           done();
         });
     });
