9a554d57f90badb0c0de450c1d2a0e33209f2a1c
Kevin Lim <lyskevin@gmail.com>
2020-09-30 11:01:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,34 +3,40 @@ let express = require('express');
 let mongoose = require('mongoose');
 let apiRoutes = require('./api-routes.js');
 
-function pokedex() {
-  let app = express();
-  
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(bodyParser.json());
-  
-  mongoose.connect('mongodb://localhost/pokedex', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  var db = mongoose.connection;
-  
-  if (db) {
-    console.log("DB connected successfully");
-  } else {
-    console.log("Error connecting DB");
-  }
+let app = express();
 
-  var port = process.env.PORT || 8080;
-  
-  app.get('/', (request, response) => response.send('Welcome to my Pokedex'));
-  app.use('/api', apiRoutes);
-  
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/pokedex', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+var db = mongoose.connection;
+
+if (db) {
+  console.log("DB connected successfully");
+} else {
+  console.log("Error connecting DB");
+}
+
+var port = process.env.PORT || 8080;
+
+app.get('/', (request, response) => response.send('Welcome to my Pokedex'));
+app.use('/api', apiRoutes);
+
+app.listen(port, function() {
+  console.log("Running Pokedex on port " + port);
+});
+
+// For testing
+module.exports = app;
+
+// For deployment
+module.exports.pokedex = function() {
   app.listen(port, function() {
     console.log("Running Pokedex on port " + port);
   });
-}
-
-module.exports.pokedex = pokedex;
+};
