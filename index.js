a70c9d703b85e6e58c5b29458c94f9bc420cf968
Kevin Lim <lyskevin@gmail.com>
2020-09-30 10:51:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,36 @@
-// Body parser allows the app to parse data from incoming requests
 let bodyParser = require('body-parser');
-
 let express = require('express');
-
-// Mongoose is a NodeJS package for modelling MongoDB.
-// It handles validation and business logic for MongoDB on NodeJS
 let mongoose = require('mongoose');
-
-let app = express();
-
-// Import routes
 let apiRoutes = require('./api-routes.js');
 
-// Configure bodyparser to handle post requests
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+function pokedex() {
+  let app = express();
+  
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+  
+  mongoose.connect('mongodb://localhost/pokedex', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  var db = mongoose.connection;
+  
+  if (db) {
+    console.log("DB connected successfully");
+  } else {
+    console.log("Error connecting DB");
+  }
 
-// Returns middleware that only parses JSON
-app.use(bodyParser.json());
-
-// Connect to Mongoose
-mongoose.connect('mongodb://localhost/pokedex', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-// Set Mongoose connection variable
-var db = mongoose.connection;
-
-// Check for DB connection
-if (db) {
-  console.log("DB connected successfully");
-} else {
-  console.log("Error connecting DB");
+  var port = process.env.PORT || 8080;
+  
+  app.get('/', (request, response) => response.send('Welcome to my Pokedex'));
+  app.use('/api', apiRoutes);
+  
+  app.listen(port, function() {
+    console.log("Running Pokedex on port " + port);
+  });
 }
 
-// Set up server port
-var port = process.env.PORT || 8080;
-
-// Send message for default URL
-app.get('/', (request, response) => response.send('Welcome to my Pokedex'));
-
-// Use API routes in the app
-app.use('/api', apiRoutes);
-
-// Launch app and listen to specified port
-app.listen(port, function() {
-  console.log("Running Pokedex on port " + port);
-});
-
-module.exports = app;
-module.exports.myFunction = app;
-
+module.exports.pokedex = pokedex;
